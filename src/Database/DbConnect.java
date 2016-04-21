f1c3b8bfd6f7d0b56b561f94fc3bf2914ed89080
FINMAL <FINMAL@FINMAL-PC>
2016-04-21 19:41:40 +0100

    All database functions are working now

--- src/Database/DbConnect.java
+++ src/Database/DbConnect.java
@@ -20,18 +20,7 @@ public class DbConnect {
     
     public Connection getConnection() throws SQLException{
         Connection conn = null;
-//        Properties connectionProps = new Properties();
-//        connectionProps.put("user", this.userName);
-//        connectionProps.put("password", this.password);
-//
-//        if (this.dbms.equals("mysql")) {
-//            conn = DriverManager.getConnection("jdbc:" + this.dbms + "://" +
-//                   this.serverName + ":" + this.portNumber + "/", connectionProps);
-//        } else if (this.dbms.equals("derby")) {
-//            conn = DriverManager.getConnection("jdbc:" + this.dbms + ":" +
-//                       this.dbName + ";create=true",connectionProps);
-//        }
-//        System.out.println("Connected to database");
+        conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/ehr", "root", "mastech2016");
         return conn;
     }
 
